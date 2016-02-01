46c56b126342784f485467a125fe96cfb526d9d7
Dong Wang <jacob.wd8816@gmail.com>
2016-02-01 16:12:53 +0800

    - add react-router

--- js/app.js
+++ js/app.js
@@ -7,9 +7,51 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+
+var browserHistory = require('react-router').browserHistory;
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Link = require('react-router').Link;
+var IndexRoute = require('react-router').IndexRoute;
+
 var XJEditorApp = require('./components/XJEditorApp.react');
 
-ReactDOM.render(
-  <XJEditorApp />,
-  document.getElementById('xjEditorApp')
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+});
+
+var Index = React.createClass({
+    render: function() {
+        var style = {
+            padding: "30px",
+        }
+        return (
+            <div>
+                <h1 style={style}>心界编辑器</h1>
+                <div>
+                    <ul>
+                        <li><Link to="/scene/haha">跳转</Link></li>
+                        <li><Link to="/scene/test" query={{ pageID: 1 }}>跳转到第一页</Link></li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Index}/>
+            <Route path="/scene/:sceneID" component={XJEditorApp} />
+        </Route>
+    </Router>
+), document.getElementById('xjEditorApp')
 );
